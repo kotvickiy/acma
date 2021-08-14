34638f4671b0194d78a9dce37bfa2db1f3fcd9d6
root <root@m.example.com>
2021-08-14 15:17:03 +0000

    commit m

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """
+________________________________
 pip install -r requirements.txt
 telegram: https://t.me/kotvickiy
+________________________________
 """
 import requests
 from bs4 import BeautifulSoup as bs
