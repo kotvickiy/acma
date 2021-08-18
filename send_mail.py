31a7a4d9f7d35d88d276776520841e7369ac8d06
kotvickiy <vladkotvickiy@gmail.com>
2021-08-18 21:24:13 +0500

    com

--- send_mail.py
+++ send_mail.py
@@ -8,7 +8,7 @@ def send_mail(list_text):
     password = 'uyOWGV6btZ3RgDL3sawv'
 
     sender = 'CheckAustralia@mail.ru'
-    recipients = ['m@wmob.eu', 'zlokovar@gmail.com', 'kotvickiy@inbox.ru']
+    recipients = ['kotvickiy@inbox.ru']  # 'm@wmob.eu', 'zlokovar@gmail.com'
     subject = 'Тест. Австралия заблокировала ещё одно казино'
     text = ''
     for i in list_text:
