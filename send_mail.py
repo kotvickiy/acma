e3671c62b6092e92ff3f3ca177d0ab0314c88f5c
root <root@m.example.com>
2021-08-19 02:52:24 +0000

    commit

--- send_mail.py
+++ send_mail.py
@@ -8,7 +8,7 @@ def send_mail(list_text, subject=''):
     password = 'uyOWGV6btZ3RgDL3sawv'
 
     sender = 'CheckAustralia@mail.ru'
-    recipients = ['kotvickiy@inbox.ru']  # 'm@wmob.eu', 'zlokovar@gmail.com'
+    recipients = ['m@wmob.eu', 'zlokovar@gmail.com', 'kotvickiy@inbox.ru']
     
     text = ''
     for i in list_text:
