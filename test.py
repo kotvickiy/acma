3b8ee11ac3fa95354a88de497c7d362395be3f68
kotvickiy <vladkotvickiy@gmail.com>
2022-05-11 12:29:01 +0500

    com

--- test.py
+++ test.py
@@ -1,7 +1,13 @@
 from main import *
 from time import sleep
+from test_ip import get_info_by_ip
+import platform
+import os
+
 
 sleep(2)
+print(get_info_by_ip())
+print(platform.system(), os.name)
 
 for i in range(10):
     with open('./sites.txt', 'r+', encoding='utf-8') as file:
--- test_ip.py
+++ test_ip.py
@@ -1,35 +1,33 @@
 import requests
+import http.client
 
 
-def get_info_by_ip(ip='127.0.0.1'):
+def get_info_by_ip():
+    conn = http.client.HTTPConnection("ifconfig.me")
+    conn.request("GET", "/ip")
+    ip = str(conn.getresponse().read())[2:-1]
+    res = []
     try:
         response = requests.get(url=f'http://ip-api.com/json/{ip}').json()
-        # print(response)
         
         data = {
             '[IP]': response.get('query'),
-            '[Int prov]': response.get('isp'),
-            '[Org]': response.get('org'),
-            '[Country]': response.get('country'),
-            '[Region Name]': response.get('regionName'),
             '[City]': response.get('city'),
-            '[ZIP]': response.get('zip'),
-            '[Lat]': response.get('lat'),
-            '[Lon]': response.get('lon'),
+            '[Org]': response.get('org')
+            
         }
         
         for k, v in data.items():
-            print(f'{k} : {v}')
+            res.append(f'{k} : {v}')
+        
+        return res
         
     except requests.exceptions.ConnectionError:
-        print('[!] Please check your connection!')
+        return '[!] Please check your connection!'
         
         
 def main():
-    ip = input('Please enter a target IP: ')
-    
-    get_info_by_ip(ip=ip)
-    
+    print(*get_info_by_ip())
     
 if __name__ == '__main__':
     main()
